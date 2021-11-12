14e135fdeacf9bc1fac299f91e4b9fb29dd2506a
Santosh Bhandari <contact@santoshb.com.np>
2021-11-12 21:58:03 +0545

    feat: Add route to get feed of a specific section

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Response,
+} from '@nestjs/common';
+import { Response as Resp } from 'express';
+
 import { FeedService } from './feed.service';
 
 @Controller('feed')
@@ -8,5 +16,19 @@ export class FeedController {
   @Get()
   getAllSections() {
     return this.feedService.getAllSections();
+  }
+
+  @Get(':section')
+  async getRssFeed(@Param('section') section: string, @Response() res: Resp) {
+    const isInputValid = /^[a-z]+(-?)[a-z]+$/gm.test(section);
+    if (!isInputValid)
+      throw new BadRequestException(
+        'Section name can include only lowercase letters and hyphens in proper format! e.g. business-to-business',
+      );
+
+    const feedContent = await this.feedService.generateFeedForSection(section);
+
+    res.set('Content-Type', 'application/xml');
+    res.end(feedContent);
   }
 }
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -2,6 +2,7 @@ import {
   HttpStatus,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -38,6 +39,69 @@ export class FeedService {
         title: item.webTitle,
       }));
   }
+
+  async generateFeedForSection(section: string) {
+    const articles = await this.getAllArticlesForSection(section);
+    return this.generateFeedFromArticles(articles);
+  }
+
+  private async getAllArticlesForSection(section: string) {
+    const response = await fetch(
+      `${GUARDIAN_API_URL}/${section}?api-key=${this.configService.get(
+        'GUARDIAN_API_KEY',
+      )}`,
+    );
+    if (response.status === HttpStatus.NOT_FOUND)
+      throw new NotFoundException(
+        'Unable to find the specified section. Check /feed to know about the possible sections',
+      );
+    else if (response.status !== HttpStatus.OK)
+      throw new InternalServerErrorException(
+        'Unable to fetch data from guardians api. Please try again later or contact the developer!',
+      );
+
+    const data = (await response.json()) as ArticleResponse;
+    if (data.response.status !== 'ok')
+      throw new InternalServerErrorException(
+        'Unable to fetch data from guardians api. Please try again later or contact the developer!',
+      );
+
+    return data.response.results;
+  }
+
+  private generateFeedFromArticles(articles: Article[]) {
+    const itemElements = articles
+      .map(
+        (post) =>
+          `
+        <item>
+          <title>${post.webTitle}</title>
+          <link>${post.webUrl}</link>
+          <guid>${post.webUrl}</guid>
+          <pubDate>${new Date(post.webPublicationDate).toUTCString()}</pubDate>
+          <category>${post.sectionName}</category>
+        </item>
+        `,
+      )
+      .join('');
+
+    const { sectionId, sectionName } = articles[0];
+
+    const rssFeed = `
+      <rss version="2.0">
+        <channel>
+          <title>The Guardian - ${sectionName}</title>
+          <link>https://www.theguardian.com/${sectionId}</link>
+          <description>${sectionName} news on UK and world from the Guardian, the world's leading liberal voice</description>
+          <language>en</language>
+          <copyright>Copyright 2021 Guardian News and Media Limited or its affiliated companies</copyright>
+          <docs>https://validator.w3.org/feed/docs/rss2.html</docs>
+          ${itemElements}
+        </channel>
+      </rss>
+    `;
+    return rssFeed;
+  }
 }
 
 export interface SectionResponse {
@@ -49,4 +113,25 @@ export interface SectionResponse {
       webTitle: string;
     }[];
   };
+}
+
+export interface ArticleResponse {
+  response: {
+    status: string;
+    total: number;
+    pageSize: number;
+    results: Article[];
+  };
+}
+
+export interface Article {
+  id: string;
+  type: string;
+  sectionId: string;
+  sectionName: string;
+  webPublicationDate: string;
+  webTitle: string;
+  webUrl: string;
+  apiUrl: string;
+  isHosted: boolean;
 }
