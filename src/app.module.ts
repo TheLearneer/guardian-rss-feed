ae13a1065d460327c57d825b02bc6e1066d69b21
Santosh Bhandari <contact@santoshb.com.np>
2021-11-13 10:23:12 +0545

    docs: Add basic info about the project
    Includes initial steps to run the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [
+    // https://docs.nestjs.com/techniques/configuration
     ConfigModule.forRoot({ isGlobal: true }),
+    // https://docs.nestjs.com/techniques/caching
     CacheModule.register({ isGlobal: true }),
     FeedModule,
   ],
